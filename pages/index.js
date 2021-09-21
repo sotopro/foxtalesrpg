15e73f1ac5d7969cd4ff90438279753397938ba2
Daniel Soto <dasg.1993@gmail.com>
2021-09-21 11:05:39 -0500

    [feature] - implement OpenSea  currency

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Page from '../components/Page'
 import * as fbq from '../lib/fpixel'
 export default function Home() {
   const handleClick = () => {
-    fbq.event('OpenSea', { url: 'https://opensea.io/collection/foxtalesrpg', value: 1, currency: 0.001 })
+    fbq.event('OpenSea', { url: 'https://opensea.io/collection/foxtalesrpg', value: 1, currency: 'USD' })
   }
   return (
     <Page>
